506c8a9716bc37715bceed70bf80bd98c01bdd49
Miles <78830914+MortifiedMiles@users.noreply.github.com>
2025-11-23 10:26:10 -0700

    Completed lesson 24, Environment Map

---
+++ 24-environment-map/src/script.js
@@ -0,0 +1,161 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+
+/**
+ * Loaders
+ */
+const gltfloader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const rgbeloader = new RGBELoader()
+
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Debug
+ */
+gui.add(scene, 'environmentIntensity').min(0).max(10).step(0.001)
+gui.add(scene, 'backgroundIntensity').min(0).max(10).step(0.001)
+gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.0001)
+gui.add(scene.backgroundRotation, 'y').min(0).max(Math.PI * 2).step(0.001)
+gui.add(scene.environmentRotation, 'y').min(0).max(Math.PI * 2).step(0.001)
+
+/**
+ * Environment Map
+ */
+scene.environmentIntensity = 1
+scene.backgroundBlurriness = 0
+scene.backgroundIntensity = 1
+scene.backgroundRotation.y = 1
+scene.environmentRotation.y = 1
+
+// LDR Cube Texture
+// const environmentMap = cubeTextureLoader.load([
+//     '/environmentMaps/2/px.png',
+//     '/environmentMaps/2/nx.png',
+//     '/environmentMaps/2/py.png',
+//     '/environmentMaps/2/ny.png',
+//     '/environmentMaps/2/pz.png',
+//     '/environmentMaps/2/nz.png'
+// ])
+
+// scene.environment = environmentMap
+// scene.background = environmentMap
+
+
+// HDR (RGBE) equirectangular
+rgbeloader.load('/environmentMaps/blender-2k.hdr', (environmentMap) => 
+    {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping 
+
+        scene.background = environmentMap
+        scene.environment = environmentMap
+})
+
+/**
+ * Real time environment map
+ */
+// const environmentMap = THREE.textureLoader.load('/environmentMaps/')
+
+
+/**
+ * Torus Knot
+ */
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+    new THREE.MeshStandardMaterial({ roughness:0.4, metalness:1, color: 0xaaaaaa})
+)
+torusKnot.position.set(-4, 4, 0)
+scene.add(torusKnot)
+
+/**
+ * Models
+ */
+gltfloader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) =>
+    {
+        gltf.scene.scale.set(10, 10, 10)
+        scene.add(gltf.scene)
+    }
+)
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(4, 5, 4)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 3.5
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+const tick = () =>
+{
+    // Time
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
